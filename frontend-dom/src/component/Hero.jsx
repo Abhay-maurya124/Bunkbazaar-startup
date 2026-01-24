9214f75f532e010da8b2c2a378ea6627c70564b7
Abhay maurya <abhaymaurya6013@gmail.com>
2026-01-24 21:02:51 +0530

    first

--- frontend-dom/src/component/Hero.jsx
+++ frontend-dom/src/component/Hero.jsx
@@ -4,7 +4,36 @@ const hero = () => {
 
   return (
     <>
+      <div className='bg-amber-100'>
+        <div className='grid grid-cols-2'>
+          <div className='m-11'>
+            <h1 className='text-6xl p-4 leading-20 font-bold'>
+              Students, Campus first market Place
+              <span className='px-7 bg-amber-400 h-10 rounded-4xl'>
+                <span className='tracking-wider'>Bunk</span>
+                <span className='tracking-widest'>Bazzar</span>
+              </span>
+            </h1>
+            <div className='flex text-2xl mt-10 gap-3.5 font-bold'>
+              <button className="h-20  w-2xs rounded-4xl  hover:bg-amber-200 
+            transition-all ease-in-out
+            bg-gradient-to-r from-[#eee] to-[#333]
+              focus:outline-none active:scale-95 ring-[#e0cc10]
+              duration-500">
+                Explore Items
+              </button>
 
+              <button className='h-20  w-2xs rounded-4xl  hover:bg-amber-200 
+            transition-all ease-in-out
+              focus:outline-none ring-4 active:scale-95 ring-[#e0cc10]
+              duration-500'>Start Yours business</button>
+            </div>
+          </div>
+          <div>
+            <img src="./original-0997d5c6b8b6f03d840209c19a9e7de4-removebg-preview.png" alt="" />
+          </div>
+        </div>
+      </div>
     </>
   )
 }
--- frontend-dom/src/component/Navbar.jsx
+++ frontend-dom/src/component/Navbar.jsx
@@ -1,23 +1,56 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import logo from '../Assets/logo.png'
 const Navbar = () => {
     return (
-        <div className='px-9 text-lg text-black   font-sans text-[#E0E5EB] bg-gradient-to-r from-[#55d]/50 to-[#3fbcfb]/50'>
-            <nav className='flex justify-between items-center'>
-                <div className='flex justify-between gap-5'>
-                    <h3>BunkBazaar</h3>
-                    <div>Location</div>
+        <div className='px-9 text-lg text-blue-100 font-semibold  font-sans  bg-gradient-to-r from-[#000]/80 to-[#04293b]'>
+            <nav className='flex justify-around items-center h-30'>
+                <div className='flex  gap-5'>
+                    <div className='flex items-center tracking-tighter'>
+                        <img src={logo} alt="" className='h-20' />
+                        <h3 className='text-yellow-400 text-4xl font-black tracking-tighter text-shadow-2xl'>Bunk <span className='text-amber-500'>Bazaar</span></h3>
+                    </div>
                 </div>
-                <input type="text" placeholder='Search' className='h-10 m-1 border-1 rounded-sm' />
+                <input type="text" placeholder='Search' className='h-10 w-90 px-3 mx-1 border-1 rounded-sm' />
                 <div>
-                    <ul className='flex justify-evenly gap-6'>
-                        <Link to="/">Home</Link>
-                        <Link to="/order">Products</Link>
-                        <Link to="/about">About</Link>
-                        <Link to="/contact" className=''>contact</Link>
-                        <button className='border-1 px-2 w-full h-10 rounded-lg '>Login</button>
-                        <button className='border-1 px-2 w-full h-10 rounded-lg '>Register</button>
+                    <ul className='flex justify-evenly items-center gap-6'>
+                        <Link to="/">
+                            <div>
+                                Home
+                            </div>
+                        </Link>
+                        <Link to="/order">
+                            <div>
+                                Products
+                            </div>
+                        </Link>
+                        <Link to="/about">
+                            <div>
+                                customer
+                            </div>
+                        </Link>
+                        <Link to="/career" className=''>
+                            <div>
+                                career
+                            </div>
+                        </Link>
+                        <Link to="/business" className=''>
+                            <div>
+                                Business
+                            </div>
+                        </Link>
+                        <Link to="/blog" className=''>
+                            <div>
+                                Blog
+                            </div>
+                        </Link>
+                        <button className="w-full h-10 px-4 rounded-lg text-white bg-amber-500 hover:bg-amber-600 transition duration-200 ease-in-out">
+                            Login
+                        </button>
+
+                        <button className="w-full h-10 px-4 rounded-lg border-2 hover:text-black hover:bg-amber-100 transition duration-200 ease-in-out">
+                            Register
+                        </button>
                     </ul>
                 </div>
             </nav>
